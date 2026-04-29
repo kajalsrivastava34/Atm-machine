ab8cc065821db043291991b438abd7a68a4c2805
Kajal kumari <kajalsrivastava422@gmail.com>
2026-04-29 19:17:12 +0530

    Update transaction.py

---
+++ transaction.py
@@ -0,0 +1,9 @@
+import transaction
+
+def show_menu():
+    print("\n--- ATM MENU ---")
+    print("1. Check Balance")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Mini Statement")
+    print("5. Exit")
